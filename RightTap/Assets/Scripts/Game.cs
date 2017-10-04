38af6fcbbf2e2ff5d05d9694652eed0bb44ea4fa
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2017-10-04 23:36:25 +0700

    + Add script for main character

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
 
-	public GameObject mcPrefab;
-	private GameObject mainCharater;
+    public GameObject mcPrefab;
+    private GameObject mainCharater;
     //private GameObject[] mMainCharacters;
 
-	// Use this for initialization
-	void Start () {
-		mainCharater = (GameObject)Instantiate(mcPrefab);
-		mainCharater.transform.position = new Vector3(0.0f, -4.0f, 0.0f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		mainCharater.transform.position += new Vector3(0.0f, 0.1f, 0.0f);
-	}
+    void Start()
+    {
+        mainCharater = (GameObject)Instantiate(mcPrefab);
 
         //mMainCharacters = new GameObject[5];
         //for(int i=0; i<5; i++)
         //{
         //    mMainCharacters[i] = (GameObject)Instantiate(mcPrefab);
         //}
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mainCharater.GetComponent<MainCharacter>().Move();
+        }
+    }
 }
