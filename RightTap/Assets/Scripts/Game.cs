009e9a57c1fa29e8255f60430869db5a7d69fb03
dung.nguyenthanh <dung.nguyenthanh@gameloft.com>
2017-10-04 18:38:18 +0700

    + Instantiate MC

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+	public GameObject mcPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		GameObject mc = (GameObject)Instantiate(mcPrefab);
 	}
 	
 	// Update is called once per frame
