42d1adb28a9c902dc3747891427bbcc952ba496c
dung.nguyenthanh <dung.nguyenthanh@gameloft.com>
2017-10-04 18:44:14 +0700

    + Make MC moving up

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Game : MonoBehaviour {
 
 	public GameObject mcPrefab;
+	private GameObject mainCharater;
 
 	// Use this for initialization
 	void Start () {
-		GameObject mc = (GameObject)Instantiate(mcPrefab);
+		mainCharater = (GameObject)Instantiate(mcPrefab);
+		mainCharater.transform.position = new Vector3(0.0f, -4.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		mainCharater.transform.position += new Vector3(0.0f, 0.1f, 0.0f);
 	}
 }
