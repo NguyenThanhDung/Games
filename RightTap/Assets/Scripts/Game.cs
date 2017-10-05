0174b8bf1eef1d213ecf904483cf362ca0de277c
dung.nguyenthanh <dung.nguyenthanh@gameloft.com>
2017-10-05 18:10:24 +0700

    + Set random number to MC when mouse click

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Game : MonoBehaviour
 {
     public GameObject mcPrefab;
     private GameObject mMainCharacter;
+	private System.Random mRandomer;
     
     void Start()
     {
 		mMainCharacter = (GameObject)Instantiate(mcPrefab);
-		mMainCharacter.GetComponent<MainCharacter>().Number = 25;
+		mRandomer = new System.Random();
     }
 
     void Update()
     {
-
+		if(Input.GetMouseButton(0))
+		{
+			int number = mRandomer.Next(0, 100);
+			mMainCharacter.GetComponent<MainCharacter>().Number = number;
+		}
     }
 }
