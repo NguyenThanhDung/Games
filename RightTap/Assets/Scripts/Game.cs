40e650bcf853435242eb1000716690b7a1e59220
dung.nguyenthanh <dung.nguyenthanh@gameloft.com>
2017-10-05 15:12:14 +0700

    - Reset all code

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    const int MAX_CHARACTERS = 10;
-
     public GameObject mcPrefab;
-    private GameObject[] mMainCharacters;
-    private int mMovingIndex;
-
+    private GameObject mMainCharacter;
+    
     void Start()
     {
-        mMainCharacters = new GameObject[MAX_CHARACTERS];
-        for (int i = 0; i < MAX_CHARACTERS; i++)
-        {
-            mMainCharacters[i] = (GameObject)Instantiate(mcPrefab);
-        }
-        mMovingIndex = -1;
+		mMainCharacter = (GameObject)Instantiate(mcPrefab);
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            mMovingIndex = (mMovingIndex >= MAX_CHARACTERS) ? 0 : mMovingIndex + 1;
-            mMainCharacters[mMovingIndex].GetComponent<MainCharacter>().Move();
-        }
+
     }
 }
--- RightTap/Assets/Scripts/MainCharacter.cs
+++ RightTap/Assets/Scripts/MainCharacter.cs
@@ -4,34 +4,8 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
-    Vector3 INITIAL_POSITION = new Vector3(0.0f, -4.0f, 0.0f);
-    const float END_Y = 6.0f;
-    private bool mIsMoving;
-
     void Start()
     {
-        this.gameObject.SetActive(false);
-        this.transform.position = INITIAL_POSITION;
-        this.mIsMoving = false;
-    }
-
-    public void Move()
-    {
-        this.gameObject.SetActive(true);
-        this.mIsMoving = true;
-    }
-
-    void FixedUpdate()
-    {
-        if (this.mIsMoving)
-        {
-            this.transform.position += new Vector3(0.0f, 0.05f, 0.0f);
-            if(this.transform.position.y > END_Y)
-            {
-                this.gameObject.SetActive(false);
-                this.transform.position = INITIAL_POSITION;
-                this.mIsMoving = false;
-            }
-        }
+        this.transform.position = new Vector3(0.0f, -3.0f, 0.0f);
     }
 }
