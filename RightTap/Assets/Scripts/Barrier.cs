2d6a0e4ec153585ec2dbaf2d9f43149c0d5ecc4e
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-07 15:18:01 +0700

    + Make barrier move down

--- RightTap/Assets/Scripts/Barrier.cs
+++ RightTap/Assets/Scripts/Barrier.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Barrier : MonoBehaviour {
+public class Barrier : MonoBehaviour
+{
+    void Start()
+    {
+        this.transform.position = new Vector3(0.0f, 5.5f);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void FixedUpdate()
+    {
+        this.transform.position -= new Vector3(0.0f, 0.05f);
+    }
 }
--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -7,9 +7,13 @@ public class Game : MonoBehaviour
 {
     public GameObject dockerObject;
     public GameObject mcObject;
+    public GameObject barrierPrefab;
+
+    private GameObject barrierObj;
 
     private Docker docker;
     private MainCharacter mainCharacter;
+    private Barrier barrier;
 
     void Start()
     {
@@ -17,6 +21,8 @@ public class Game : MonoBehaviour
 
         this.docker = dockerObject.GetComponent<Docker>();
         this.mainCharacter = mcObject.GetComponent<MainCharacter>();
+
+        this.barrierObj = (GameObject)Instantiate(barrierPrefab);
     }
 
     void Update()
