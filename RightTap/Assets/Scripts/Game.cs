053f046e51365d5e68008b27e0a4f606e72f5fd3
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-07 11:32:56 +0700

    - Use space for indent characters

--- RightTap/Assets/Scripts/Game.cs
+++ RightTap/Assets/Scripts/Game.cs
@@ -6,21 +6,21 @@ using System;
 public class Game : MonoBehaviour
 {
     public GameObject Docker;
-    public GameObject MainCharacter;    
-	private System.Random mRandomer;
-    
+    public GameObject MainCharacter;
+    private System.Random mRandomer;
+
     void Start()
     {
         Application.targetFrameRate = 30;
-		mRandomer = new System.Random();
+        mRandomer = new System.Random();
     }
 
     void Update()
     {
-		if(Input.GetMouseButton(0))
-		{
-			int number = mRandomer.Next(0, 100);
+        if (Input.GetMouseButton(0))
+        {
+            int number = mRandomer.Next(0, 100);
             MainCharacter.GetComponent<MainCharacter>().Number = number;
-		}
+        }
     }
 }
--- RightTap/Assets/Scripts/MainCharacter.cs
+++ RightTap/Assets/Scripts/MainCharacter.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
-	private int mNumber;
-	public int Number
-	{
-		set
-		{
-			if(mNumber!=value)
-			{
-				mNumber = value;
-				this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = mNumber.ToString();
-			}
-		}
-	}
+    private int mNumber;
+    public int Number
+    {
+        set
+        {
+            if (mNumber != value)
+            {
+                mNumber = value;
+                this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = mNumber.ToString();
+            }
+        }
+    }
 
     void Start()
     {
-        
+
     }
 }
