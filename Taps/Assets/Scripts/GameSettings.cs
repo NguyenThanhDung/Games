f25493477688c40efb1ec64e15c5636711a260d4
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-18 21:53:12 +0700

    + Init Taps project

---
+++ Taps/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : ScriptableObject
+{
+    [System.Serializable]
+    public class Level
+    {
+        public int param;
+
+        public Level()
+        {
+            param = 0;
+        }
+    }
+
+    public List<Level> levels = new List<Level>(0);
+
+    public GameSettings()
+    {
+        levels.Add(new Level());
+    }
+}
