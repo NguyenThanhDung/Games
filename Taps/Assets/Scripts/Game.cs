044cf6e75d847cd5861dfa5d20ed78d24105280c
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-19 22:31:42 +0700

    - Set up screen size

--- Taps/Assets/Scripts/Game.cs
+++ Taps/Assets/Scripts/Game.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_EDITOR || UNITY_WEBGL
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+#else
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 30;
+#endif
+        Screen.SetResolution(480, 800, false);
+    }
+    
+    void Update()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
