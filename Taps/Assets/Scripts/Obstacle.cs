f0141a51394045e0d123c94d4d849342f39840c2
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-21 11:14:35 +0700

    - Adjust obstacle size

--- Taps/Assets/Scripts/Obstacle.cs
+++ Taps/Assets/Scripts/Obstacle.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float LengthUnit = 0.8f;
+    public float LengthUnit;    
 
     public int _hp;
     public int _length = 3;
     public int _space;
 
+    private float OBSTACLE_WIDTH_BUFFER = 0.5f;
+    private float CIRCLE_SCALE = 0.6f;
     private TextMesh _hpText;
         
     void Start()
     {
-        float width = Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height + 0.5f;
+        float width = Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height + OBSTACLE_WIDTH_BUFFER;
         float height = _length * LengthUnit;
         transform.localScale = new Vector3(width, height, 1.0f);
         for (int i = 0; i < transform.childCount; i++)
         {
-            Vector3 childScale = new Vector3(1.0f / transform.localScale.x, 1.0f / transform.localScale.y, 1.0f);
+            Vector3 childScale = new Vector3(CIRCLE_SCALE / transform.localScale.x, CIRCLE_SCALE / transform.localScale.y, 1.0f);
             transform.GetChild(i).transform.localScale = childScale;
         }
 
