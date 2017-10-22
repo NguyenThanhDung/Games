f8e92e9c189c647f25bca0a160548930bc61264f
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-22 11:55:52 +0700

    + Display LevelEditor on Inspector

--- Taps/Assets/Scripts/Editor/LevelEditor.cs
+++ Taps/Assets/Scripts/Editor/LevelEditor.cs
@@ -11,9 +11,61 @@ public class LevelEditor : Editor
         serializedObject.Update();
         SerializedProperty levels = serializedObject.FindProperty("levels");
 
-        if (GUILayout.Button("Add"))
+        for (int i = 0; i < levels.arraySize; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Foldout(true, "Level " + (i + 1).ToString());
+            if (GUILayout.Button("Remove Level", GUILayout.Width(100)))
+            {
+                SerializedProperty childObstacleDatas = serializedObject.FindProperty("levels.Array.data[" + i + "].obstacleDatas");
+                childObstacleDatas.DeleteCommand();
+                levels.DeleteArrayElementAtIndex(i);
+                break;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            SerializedProperty obstacleDatas = serializedObject.FindProperty("levels.Array.data[" + i + "].obstacleDatas");
+            EditorGUI.indentLevel++;
+            for (int j = 0; j < obstacleDatas.arraySize; j++)
+            {
+                SerializedProperty obstacleData = obstacleDatas.GetArrayElementAtIndex(j);
+                SerializedProperty obstacleData_hp = serializedObject.FindProperty("levels.Array.data[" + i + "].obstacleDatas.Array.data[" + j + "].hp");
+                SerializedProperty obstacleData_length = serializedObject.FindProperty("levels.Array.data[" + i + "].obstacleDatas.Array.data[" + j + "].length");
+                SerializedProperty obstacleData_space = serializedObject.FindProperty("levels.Array.data[" + i + "].obstacleDatas.Array.data[" + j + "].space");
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Template " + (j + 1).ToString(), GUILayout.Width(100));
+                EditorGUILayout.IntField(obstacleData_hp.intValue, GUILayout.Width(50));
+                EditorGUILayout.IntField(obstacleData_length.intValue, GUILayout.Width(50));
+                EditorGUILayout.IntField(obstacleData_space.intValue, GUILayout.Width(50));
+                EditorGUILayout.Space();
+                if (GUILayout.Button("-", GUILayout.Width(30)))
+                {
+                    obstacleDatas.DeleteArrayElementAtIndex(j);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Space();
+            if (GUILayout.Button("+", GUILayout.Width(30)))
+            {
+                obstacleDatas.InsertArrayElementAtIndex(obstacleDatas.arraySize);
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUI.indentLevel--;
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+        }
+
+        if (GUILayout.Button("Add Level"))
         {
             levels.InsertArrayElementAtIndex(levels.arraySize);
+            SerializedProperty obstacleDatas = serializedObject.FindProperty("levels.Array.data[" + (levels.arraySize - 1) + "].obstacleDatas");
+            obstacleDatas.ClearArray();
+            obstacleDatas.InsertArrayElementAtIndex(obstacleDatas.arraySize);
         }
 
         serializedObject.ApplyModifiedProperties();
