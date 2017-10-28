37a29aa7584c08b107158d007ca153fe67400dac
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-28 12:57:28 +0700

    + Instantiate enemy

---
+++ HurryTaps/Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject _enemyPrefab;
+
+    void Start()
+    {
+        Instantiate(_enemyPrefab);
+    }
+
+    void Update()
+    {
+
+    }
+}
