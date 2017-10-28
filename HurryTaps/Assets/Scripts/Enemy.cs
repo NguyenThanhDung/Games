66bd076ba01027634a67145e1a6c7b3c5d4e978d
Unknown <thanhdungspk@yahoo.com.vn>
2017-10-28 13:19:41 +0700

    + Add enemy script

---
+++ HurryTaps/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject _redZone;
+
+    private float _timeOut;
+
+    void Start()
+    {
+        _timeOut = 1.0f;
+
+        float positionY = _timeOut / 2 - 0.5f;
+        float scaleY = _timeOut;
+
+        _redZone.transform.position = new Vector3(0.0f, positionY, -0.1f);
+        _redZone.transform.localScale = new Vector3(1.0f, scaleY, 1.0f);
+    }
+}
